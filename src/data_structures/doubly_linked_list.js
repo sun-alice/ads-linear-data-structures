bc77a182a00efc0ae449df8f6904adfc134f864d
Alice <alicesunhi@gmail.com>
2020-05-25 20:05:50 -0700

    m

--- src/data_structures/doubly_linked_list.js
+++ src/data_structures/doubly_linked_list.js
@@ -1,5 +1,10 @@
 class DLLNode {
-  constructor({ element = undefined, next = this, prev = this, isSentinel = false }) {
+  constructor({
+    element = undefined,
+    next = this,
+    prev = this,
+    isSentinel = false,
+  }) {
     this.element = element;
     this.next = next;
     this.prev = prev;
@@ -31,24 +36,69 @@ class DoublyLinkedList {
   }
 
   insertHead(element) {
+    // const newHead = new this.Node(element);
+    // newHead.next = this._head();
+    // newHead.prev = this._sentinel;
+    // this._head().prev = newHead;
+    // this._sentinel.next = newHead;
+
+    // return newHead;
+    const node = new this.Node({
+      element,
+      next: this._head(),
+      prev: this._sentinel,
+    });
+    this._head().prev = node;
+    this._sentinel.next = node;
+    return node;
   }
 
   insertTail(element) {
+    // const newTail = new thjo(element);
+    // newTail.prev = this._tail();
+    // newTail.next = this._sentinel;
+    // this._tail().next = newTail;
+    // this._sentinel.prev = newTail;
+
+    // return newTail;
+
+    const node = new this.Node({
+      element,
+      next: this._sentinel,
+      prev: this._tail(),
+    });
+    this._tail().next = node;
+    this._sentinel.prev = node;
+    return node;
   }
 
   removeHead() {
+    return this._head().remove();
   }
 
   removeTail() {
+    return this._tail().remove();
   }
 
   remove(node) {
+    if (node.remove) {
+      return node.remove();
+    }
   }
 
-  forEach(callback) {
+  forEach(callback, container = this) {
+    let i = 0;
+    let node = this._head();
+    while (node !== this._sentinel) {
+      callback(node.element, i, container);
+      i += 1;
+      node = node.next;
+    }
   }
-
   count() {
+    let count = 0;
+    this.forEach(() => (count += 1));
+    return count;
   }
 }
 
